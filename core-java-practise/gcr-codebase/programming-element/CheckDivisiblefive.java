5113f5fdf38b1ce29dfca89c2c4d5bf057d94c06
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2025-12-20 10:45:58 +0530

    Divisiblebyfive

--- core-java-practise/gcr-codebase/programming-element/CheckDivisiblefive.java
+++ core-java-practise/gcr-codebase/programming-element/CheckDivisiblefive.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CheckDivisibleFive {
+public class CheckDivisiblefive {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
