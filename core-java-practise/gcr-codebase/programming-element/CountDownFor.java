87e77a79862842ec11864b8a18ce1ef1af1f4482
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2025-12-20 11:05:08 +0530

    CountdownFor

--- core-java-practise/gcr-codebase/programming-element/CountDownFor.java
+++ core-java-practise/gcr-codebase/programming-element/CountDownFor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CountdownFor {
+public class CountDownFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int counter = sc.nextInt();
